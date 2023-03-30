50a458788f4555f530d9b41a787ebde818415ac8
karstro <karst@makajelaan.nl>
2023-03-30 11:30:41 +0200

    Add project files.

---
+++ Leden.cs
@@ -0,0 +1,11 @@
+namespace FlorisoftTennisApp {
+    public partial class Leden : Form {
+        public Leden() {
+            InitializeComponent();
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e) {
+
+        }
+    }
+}
